02ccf05acb2c24238d00b209b4a3e2475dc300cb
Jithin <65081177+jithinb68@users.noreply.github.com>
2022-01-02 11:37:45 +0530

    Time: 207 ms (5.65%), Space: 46.2 MB (45.18%) - LeetHub

---
+++ find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function(words) {
+    let palindromicString = "";
+    for(const word of words) {
+        if(word === word.split('').reverse().join('')) {
+            palindromicString = word;
+            break;
+        }
+    }
+    return palindromicString
+};
