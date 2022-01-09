2232416feffef4263c11f723077e15d080e62086
Jithin <65081177+jithinb68@users.noreply.github.com>
2022-01-09 14:20:06 +0530

    Time: 99 ms (17.68%), Space: 38.7 MB (75.25%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.js
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -3,14 +3,5 @@
  * @return {number}
  */
 var maximum69Number  = function(num) {
-    let result = [];
-    const numArr =  String(num).split('');
-    const numArrLen = numArr.length;
-    for(let i=0; i<numArrLen; i++){
-        if(numArr[i] === '6') {
-            numArr[i] = 9;
-            break;
-        }
-    }
-    return numArr.join('');
+    return Number(num.toString().replace('6', '9'));
 };
