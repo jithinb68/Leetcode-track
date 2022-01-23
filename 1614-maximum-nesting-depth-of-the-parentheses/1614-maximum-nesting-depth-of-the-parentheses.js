2453b2b4fd73f52d25c457482b94c6306ae1cd22
Jithin <65081177+jithinb68@users.noreply.github.com>
2022-01-24 03:36:19 +0530

    Time: 124 ms (17.86%), Space: 40.3 MB (9.23%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    if(s === '()') return 1;
+   let maxNestingDepth = 0;
+   let currMaxDepth = 0;
+   let stack = [];
+   let hashMap = {}
+   let strArr = s.split('');
+    for(const item of strArr) {
+        if(item === '(') {
+            stack.push(item);
+            maxNestingDepth++;
+            if(maxNestingDepth > currMaxDepth) {
+                currMaxDepth = maxNestingDepth;
+            }
+        } else if(item === ')') {
+            stack.pop();
+            maxNestingDepth--;
+        } else {
+            if(maxNestingDepth > currMaxDepth) {
+                currMaxDepth = maxNestingDepth;
+            }
+        }
+    }
+    return currMaxDepth;
+};
