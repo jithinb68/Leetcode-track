570d5e22846dc738cf83b9a8cb57b2c8b1dc73c3
Jithin <65081177+jithinb68@users.noreply.github.com>
2022-01-30 12:49:25 +0530

    Time: 149 ms (5.36%), Space: 41.8 MB (7.14%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.js
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.js
@@ -10,20 +10,17 @@ var timeRequiredToBuy = function(tickets, k) {
         tickets[0]--;
         time++;
         if(tickets[0] === 0) {
-            // console.log('if' + k);
             if(k===0) {
                 kPopped = true;
             }
             tickets.shift();
         } else {
-            // console.log('else' + k);
-            tickets.push(tickets.shift());
             if(k===0) {
                 k = tickets.length;
             }
+            tickets.push(tickets.shift());
         }
         k--;
-        // console.log(tickets);
     }
    return time;
 };
