4c492b61b3cd3883f81dc05e75f1fc522a43a383
Jithin <65081177+jithinb68@users.noreply.github.com>
2022-02-09 23:19:42 +0530

    Time: 111 ms (12.03%), Space: 41.6 MB (99.17%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
@@ -4,6 +4,6 @@
  */
 var minimumSum = function(num) {
     let digits = num.toString().split('');
-    digits = digits.sort()
+    digits = digits.sort();
     return parseInt(digits[0] + digits[3]) + parseInt(digits[1] + digits[2]);
 };
