35d96c397281757cca3d4bc6572e14ed8726ce4b
Jithin <65081177+jithinb68@users.noreply.github.com>
2022-04-16 03:15:43 +0530

    Time: 89 ms (77.54%), Space: 44.2 MB (76.43%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -4,7 +4,5 @@
  */
 var smallerNumbersThanCurrent = function(nums) {
    const sorted = [...nums].sort((a, b) => a - b);
-   // const map = new Map(sorted.map((num, index) => [num, nums.length - index - 1]));
-   //  console.log(map)
    return nums.map(num => sorted.indexOf(num));
 };
