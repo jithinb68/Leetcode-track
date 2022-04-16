901816b3ec8f66bab3de7ab06da29c1a468e01fe
Jithin <65081177+jithinb68@users.noreply.github.com>
2022-04-17 01:53:02 +0530

    Time: 111 ms (78.62%), Space: 44.9 MB (84.26%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -6,14 +6,16 @@ var firstUniqChar = function(s) {
    let unique = s.substring(0, 1);
    let letterMap = new Map()
    for(const itm of s) {
-       if(!letterMap.has(itm)) {
-          letterMap.set(itm, 1)
+       if(!letterMap[itm]) {
+          letterMap[itm] = 1;
        } else {
-          letterMap.set(itm, letterMap.get(itm) +1)
+           letterMap[itm]++
        }
    }
-   unique = [...letterMap.entries()].find((item) => item[1] === 1)
-   return unique ? s.indexOf(unique[0]) : -1;
+    for(let i = 0; i < s.length; i++) {
+        if(letterMap[s[i]] === 1) return i
+    }    
+    return -1
 };
 
 
@@ -44,19 +46,3 @@ var firstUniqChar = function(s) {
 
 
 
-//  let occuranceHashMapWithIndex = {};
-//     let strArr = s.split
-//     for(let i= 0; i<strArr.length; i++) {
-//         if(occuranceHashMapWithIndex[strArr[i]]) {
-//             occuranceHashMapWithIndex[strArr[i]].numberOfTimes++
-//         } else {
-//             occuranceHashMapWithIndex[strArr[i]] = {firstIndex: i, numberOfTimes: 1}
-//         }
-//     }
-//     let filteredJson = []
-//     for(const item in occuranceHashMapWithIndex) {
-//        if(occuranceHashMapWithIndex[item].numberOfTimes === 1) {
-//            filteredJson.push(occuranceHashMapWithIndex[item])
-//        }
-//     }
-//     return filteredJson.length ? filteredJson[0].firstIndex : -1;
