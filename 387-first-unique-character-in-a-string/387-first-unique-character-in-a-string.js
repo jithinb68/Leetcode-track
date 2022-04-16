f2882005d8bcf63350b765ebbe7295630488f6b8
Jithin <65081177+jithinb68@users.noreply.github.com>
2022-04-17 01:44:18 +0530

    Time: 115 ms (75.48%), Space: 44.7 MB (91.76%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -3,20 +3,60 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let occuranceHashMapWithIndex = {};
-    let strArr = s.split('');
-    for(let i= 0; i<strArr.length; i++) {
-        if(occuranceHashMapWithIndex[strArr[i]]) {
-            occuranceHashMapWithIndex[strArr[i]].numberOfTimes++
-        } else {
-            occuranceHashMapWithIndex[strArr[i]] = {firstIndex: i, numberOfTimes: 1}
-        }
-    }
-    let filteredJson = []
-    for(const item in occuranceHashMapWithIndex) {
-       if(occuranceHashMapWithIndex[item].numberOfTimes === 1) {
-           filteredJson.push(occuranceHashMapWithIndex[item])
+   let unique = s.substring(0, 1);
+   let letterMap = new Map()
+   for(const itm of s) {
+       if(!letterMap.has(itm)) {
+          letterMap.set(itm, 1)
+       } else {
+          letterMap.set(itm, letterMap.get(itm) +1)
        }
-    }
-    return filteredJson.length ? filteredJson[0].firstIndex : -1;
+   }
+   unique = [...letterMap.entries()].find((item) => item[1] === 1)
+   return unique ? s.indexOf(unique[0]) : -1;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  let occuranceHashMapWithIndex = {};
+//     let strArr = s.split
+//     for(let i= 0; i<strArr.length; i++) {
+//         if(occuranceHashMapWithIndex[strArr[i]]) {
+//             occuranceHashMapWithIndex[strArr[i]].numberOfTimes++
+//         } else {
+//             occuranceHashMapWithIndex[strArr[i]] = {firstIndex: i, numberOfTimes: 1}
+//         }
+//     }
+//     let filteredJson = []
+//     for(const item in occuranceHashMapWithIndex) {
+//        if(occuranceHashMapWithIndex[item].numberOfTimes === 1) {
+//            filteredJson.push(occuranceHashMapWithIndex[item])
+//        }
+//     }
+//     return filteredJson.length ? filteredJson[0].firstIndex : -1;
