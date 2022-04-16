b08114834dc731974d29b91777854b2af533d280
Jithin <65081177+jithinb68@users.noreply.github.com>
2022-04-16 13:23:31 +0530

    Time: 59 ms (99.72%), Space: 46.1 MB (85.64%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -3,12 +3,20 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    let reachable = 0;
-    for(let i=0; i<nums.length; i++) {
-       if(reachable < i) {
-           return false
-       }
-       reachable = Math.max(reachable, i + nums[i])
+    let goal = nums.length-1;
+    for(let i=nums.length-1; i>=0; i--) {
+        if(i + nums[i] >= goal) {
+          goal = i;
+        }
     }
-    return true;
+    return goal === 0;
 };
+
+// let reachable = 0;
+// for(let i=0; i<nums.length; i++) {
+//    if(reachable < i) {
+//        return false
+//    }
+//    reachable = Math.max(reachable, i + nums[i])
+// }
+// return true;
