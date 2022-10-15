325120046348ddf5671bf79e2c7cd8285dae4872
Jithin <65081177+jithinb68@users.noreply.github.com>
2022-10-15 10:03:46 +0530

    Time: 955 ms (20.00%), Space: 81.5 MB (5.26%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.js
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.js
@@ -3,25 +3,22 @@
  * @return {number}const isReplaced = false;
  */
 var secondsToRemoveOccurrences = function(s) {
-   let secondsTaken = -1;
-   s = s.split('');
-   function replaceFindings(s) {
-        secondsTaken++;
-        const sLen = s.length;
-        let isReplaced = false;
-        for(let i=0; i<=sLen; i++) {
-            if(i < s.length-1 && s[i] < s[i+1]) {
-                isReplaced = true;
-                s[i] = '1';
-                s[i+1] = '0';
-                i = i+1;
+   let cnt = -1
+   function count(s){
+        cnt++
+        str = ""
+        let isvalid = true
+        for(let i=0; i<s.length; i++){
+            if(i < s.length-1 && s[i] < s[i+1]){
+                isvalid = false
+                str += s[i+1]
+                str += s[i]
+                i++;
+            }else if(i < s.length){
+                str += s[i];
             }
         }
-        if(isReplaced) {
-            isReplaced = false;
-            replaceFindings(s)
-        }
-        return secondsTaken
+        return isvalid == true ? cnt : count(str);
     }
-    return replaceFindings(s);
+    return count(s)
 };
